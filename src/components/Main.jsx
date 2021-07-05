28c33dfb585091f1bf4c63286fb484743409de6a
na4095 <saadmrabet008@gmail.com>
2021-07-05 21:46:28 +0100

    The first design of the website

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,68 @@
+import React from 'react';
+import { Typography, Container, CssBaseline, Grid, Avatar, Box } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles((theme) => ({
+    square: {
+      color: 'white',
+      backgroundColor: 'green',
+      width: 50,
+      height: 50
+    },
+    box1: {
+      color: 'white',
+      backgroundColor: 'red',
+      padding: 5,
+      width: 25
+    },
+    box2: {
+        color: 'white',
+        backgroundColor: 'yellow',
+        padding: 5,
+        width: 25
+      }
+  }));
 
 export default function Main() {
+    const classes = useStyles();
     return (
-        <>
-        <p>Hello world !</p>
-        </>
+        <React.Fragment>
+          <CssBaseline />
+          <Container>
+            <Grid Container>
+              <Grid item xs={4}>
+                <Avatar variant="square" className={classes.square}>
+                    N
+                </Avatar>
+              </Grid>
+              <Grid item xs={8}>
+                  <Typography variant="h4" gutterBottom>
+                      Repository Name
+                  </Typography>
+                  <Typography variant="subtitle1" gutterBottom>
+                      Repository Description
+                  </Typography>
+                  <Box
+                    display="flex"
+                    alignItems="flex-start"
+                    p={1}
+                    m={1}
+                    bgcolor="background.paper"
+                    css={{ height: 100 }}
+                   >
+                    <Box display="flex" className={classes.box1}>
+
+                    </Box>
+                    <Box  display="flex" className={classes.box2}>
+
+                    </Box>
+                    <Typography variant="subtitle1" gutterBottom>
+                        Repository Description
+                    </Typography>
+                </Box>
+              </Grid>
+            </Grid>
+          </Container>
+        </React.Fragment>
     )
 }
