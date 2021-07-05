875a2e7f2e197b93eeb09a75d9fa9276eb57cc96
na4095 <saadmrabet008@gmail.com>
2021-07-05 21:56:31 +0100

    upgread the style

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,26 +1,36 @@
 import React from 'react';
-import { Typography, Container, CssBaseline, Grid, Avatar, Box } from '@material-ui/core';
+import { Typography, CssBaseline, Avatar, Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
+    root: {
+        padding: 25,
+        margin: 0,
+    },
+    content: {
+        padding: 25,
+
+    },
     square: {
       color: 'white',
       backgroundColor: 'green',
-      width: 50,
-      height: 50
+      width: 170,
+      height: 170
     },
     box1: {
       color: 'white',
       backgroundColor: 'red',
-      padding: 5,
-      width: 25
+      padding: 15,
+      width: 75
     },
     box2: {
-        color: 'white',
-        backgroundColor: 'yellow',
-        padding: 5,
-        width: 25
-      }
+      color: 'white',
+      backgroundColor: 'yellow',
+      marginRight: 10,
+      marginLeft: 10,
+      padding: 15,
+      width: 75
+    },
   }));
 
 export default function Main() {
@@ -28,41 +38,42 @@ export default function Main() {
     return (
         <React.Fragment>
           <CssBaseline />
-          <Container>
-            <Grid Container>
-              <Grid item xs={4}>
-                <Avatar variant="square" className={classes.square}>
-                    N
-                </Avatar>
-              </Grid>
-              <Grid item xs={8}>
-                  <Typography variant="h4" gutterBottom>
-                      Repository Name
-                  </Typography>
+            <Box
+              display="flex"
+              alignItems="flex-start"    
+              p={1}
+              m={1}
+              className={classes.root}
+              >
+            <Avatar variant="square" className={classes.square}>
+                N
+            </Avatar>
+
+           <Box className={classes.content}>
+              <Typography variant="h4" gutterBottom>
+                Repository Name
+              </Typography>
+              <Typography variant="subtitle1" gutterBottom>
+                  Repository Description
+              </Typography>
+              <Box
+                display="flex"
+                alignItems="flex-start"
+                p={1}
+                m={1}
+                bgcolor="background.paper"
+                css={{ height: 100 }}
+                >
+                  <Box display="flex" className={classes.box1}>
+                  </Box>
+                  <Box  display="flex" className={classes.box2}>
+                  </Box>
                   <Typography variant="subtitle1" gutterBottom>
                       Repository Description
                   </Typography>
-                  <Box
-                    display="flex"
-                    alignItems="flex-start"
-                    p={1}
-                    m={1}
-                    bgcolor="background.paper"
-                    css={{ height: 100 }}
-                   >
-                    <Box display="flex" className={classes.box1}>
-
-                    </Box>
-                    <Box  display="flex" className={classes.box2}>
-
-                    </Box>
-                    <Typography variant="subtitle1" gutterBottom>
-                        Repository Description
-                    </Typography>
                 </Box>
-              </Grid>
-            </Grid>
-          </Container>
+              </Box>
+            </Box>
         </React.Fragment>
     )
 }
